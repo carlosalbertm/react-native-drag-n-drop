20c133a910a01cf637896ddff9e89227d323a92f
Carlos M <alberto.meza.s@gmail.com>
2018-10-10 11:33:50 -0500

    Toast test

--- android/src/main/java/co/buckets/modules/dnd/RNDragNDropPackage.java
+++ android/src/main/java/co/buckets/modules/dnd/RNDragNDropPackage.java
@@ -1,21 +1,25 @@
 
 package co.buckets.modules.dnd;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 
 public class RNDragNDropPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     return Arrays.<NativeModule>asList(new RNDragNDropModule(reactContext));
+    // List<NativeModule> modules = new ArrayList<>();
+    // modules.add(new RNDragNDropModule(reactContext))
+    // return modules;
   }
 
   // Deprecated from RN 0.47
@@ -25,7 +29,6 @@ public class RNDragNDropPackage implements ReactPackage {
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    RNDragNDropManager dragNDropManager = new RNDragNDropManager();
-    return Arrays.<ViewManager>asList(dragNDropManager);
+    return Collections.emptyList();
   }
 }
